8f55ecd1f9d53e349091f1011e59ade6a8ad32d0
Craig Iannello <caiannello@gmail.com>
2024-09-30 21:27:13 -0400

    Create bios_func_tab.d

---
+++ Software/6309/Demos/bios_func_tab.d
@@ -0,0 +1,21 @@
+;------------------------------------------------------------------------------
+; BIOS functions jump table
+;------------------------------------------------------------------------------
+FUNC_BASE       equ  $FEA0
+;------------------------------------------------------------------------------
+BF_V_RESET      equ  FUNC_BASE+0
+BF_UT_GETC      equ  FUNC_BASE+3
+BF_UT_GETS      equ  FUNC_BASE+6
+BF_UT_PUTC      equ  FUNC_BASE+9
+BF_UT_PUTS      equ  FUNC_BASE+12
+BF_UT_WAITTX    equ  FUNC_BASE+15
+BF_UT_CLRSCR    equ  FUNC_BASE+18
+BF_S_HEXA       equ  FUNC_BASE+21	; Convert Reg A to hex octet at X, X+=2
+BF_S_EOL        equ  FUNC_BASE+24   ; Put CR, LF, $00 at X, X+=3
+BF_S_LEN        equ  FUNC_BASE+27   ; Put len of string at X into Reg B
+BF_S_CPY        equ  FUNC_BASE+30   ; Copy string at Y to mem at X. X+=?
+BF_RTC_GETTIX   equ  FUNC_BASE+33   ; Put tickcount to hex string at X, X+=17
+BF_S_TOUPPER    equ  FUNC_BASE+36   ; Make A uppercase if it is lowercase
+BF_S_INTD       equ  FUNC_BASE+39   ; Convert reg D to string at X, X+=?
+BF_RTC_SETTIX   equ  FUNC_BASE+42   ; SET CURRENT SYSTEM TICKS FROM I64 AT X
+;------------------------------------------------------------------------------
